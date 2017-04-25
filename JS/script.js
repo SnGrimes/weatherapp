de4508ca74250b4b5a5256fd223372d52af72c2d
Shantia <shantiagrimes@gmail.com>
2017-04-25 18:44:31 -0500

    code edits and obj pulling data

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,4 @@
 'use strict'
-var rawTemp;
-var rawHigh;
-var rawLow;
 
 window.onload = function getLoc() {
     navigator.geolocation.getCurrentPosition(function(position){
@@ -9,147 +6,110 @@ window.onload = function getLoc() {
         var userLong = position.coords.longitude;
         console.log('We got a location! Lat:' + userLat + ' Lon: ' + userLong);
 
-        var request = new XMLHttpRequest();
-        var FiveDayReq = new XMLHttpRequest();
-        // Since open weather is an http you can use 'https://cors-anywhere.herokuapp.com/' right before url to get https for cross origin errors
-request.open('GET', 'response.json', true);
-//request.open('GET', 'http://api.openweathermap.org/data/2.5/weather?lat=' + userLat +'&lon=' + userLong +'&appid=549ea4e9fb7d89a512a515156a787ab8', true);
-//City Dallas: 4684888
-request.onload = function() {
-    if(request.status >= 200 && request.status <400) {
-        //Success
-        var data = JSON.parse(request.responseText);
-        displayData(data);
-        rawTemp = data.main.temp;
-        rawHigh = data.main.temp_max;
-        rawLow = data.main.temp_min;
-    } else {
-        console.log('There was an error when current weather data.')
-    }
-};
-
-FiveDayReq.open('GET', '5day_response.json', true);
-FiveDayReq.onload = function() {
-    if(FiveDayReq.status >= 200 && FiveDayReq.status <400) {
-        //Success
-        var data = JSON.parse(FiveDayReq.responseText);
-        display5Day(data);
-    } else {
-        console.log('There was an error returning 5 day forecast data.')
-    }
-};
-
-request.onerror = function() {
-    console.log('There was a connection error.')
-}
-FiveDayReq.onerror = function() {
-    console.log('There was a connection error (5day)')
-}
-
-request.send();
-FiveDayReq.send();
-
-    });
-}
-
-function displayData (data) {
-    var weatherCity = data.name;
-    var weatherId = data.weather[0].id;
-    var weatherDesc = data.weather[0].description;
-    var weatherParams = data.weather[0].main;
-    var weatherTemp = data.main.temp;
-    var weatherHumid = data.main.humidity;
-    var tempHigh = data.main.temp_max;
-    var tempLow = data.main.temp_min;
-    var wndSpd = data.wind.speed;
-    var wndDeg = data.wind.deg;
-
-    var initialTemp = (weatherTemp * (9/5)) - 459.67;
-    var initialHigh = (tempHigh * (9/5)) - 459.67;
-    var initialLow = (tempLow * (9/5)) - 459.67;
-
-    document.getElementById('temp_display').innerHTML = "<p>" + initialTemp.toPrecision(4) + "<p>";
-    document.getElementById('humidity_display').innerHTML= "<p>" + weatherHumid + "<p>";
-    document.getElementById('temphi_display').innerHTML= "<p>" + initialHigh.toPrecision(4) + "<p>";
-    document.getElementById('templo_display').innerHTML= "<p>" + initialLow.toPrecision(4) + "<p>";
-    document.getElementById('city_display').innerHTML= "<p>" + weatherCity + "<p>";
-    weatherCodes(weatherId);
-}
-
-function weatherCodes (code) {
-    var display = document.getElementById('weather_display');
-    if (code >= 200 && code <= 232 ) {
-        display.innerHTML = "<p>This is a thunderstorm</p>";
-    }
-    else if (code >= 300 && code <= 321) {
-        display.innerHTML = "<p>This is drizzle</p>";
-    }
-    else if (code >= 500 && code <= 531) {
-        display.innerHTML = "<p>This is rain</p>";
-    }
-    else if (code >= 600 && code <= 622) {
-        display.innerHTML = "<p>This is Snow</p>";
-    }
-    else if (code == 800) {
-        display.innerHTML = "<p>This is clear sky</p>"; 
-    }
-    else if (code >= 801 && code <= 804) {
-        display.innerHTML = "<p>It is cloudy</p>"; 
-    }
-    else if (code >= 900 && code <= 906) {
-        display.innerHTML = "<p>Omg! Weather!</p>"; 
-    }
-}
-
-function display5Day (data) {
-    console.log(data);
-    var fiveDay = document.getElementsByClassName('day_fill');
-    var fiveDate = document.getElementsByClassName('date_fill');
+        api.connection('GET', 'response.json');
+        api.displayData();
     
-    var dataDay = 1;
-    for (var j = 0; j < fiveDay.length; j++) {
-        fiveDay[j].textContent = data.list[dataDay].main.temp + " " + data.list[dataDay].weather[0].id;
-        dataDay += 8;
-    }
-    var dataDate = 1;
-    for (var i = 0; i < fiveDate.length; i++) {
-        fiveDate[i].textContent = data.list[dataDate].dt_txt;
-        dataDate += 8;
-    }
-}
+    }); 
+} /** End of Onload sequence **/
 
-var FC = true;
 
-function toggleUnit() {
-    var temp = 0;
-    var hi = 0;
-    var lo = 0;
-    var temperature = parseInt(rawTemp);
-    var high = parseInt(rawHigh);
-    var low = parseInt(rawLow);
-    var button = document.getElementById('unit_switch');
+var api = {
+    FC: true,
+    set setFC (value) {
+        this.FC = value;
+    },
+    get getFC() {
+        return this.FC;
+    },
+    setButton: function(letter) {
+        var button = document.getElementById('unit_switch');
+        button.value = letter;
+    },
+    connection: function (type, url) {
+            var request = new XMLHttpRequest();
+            request.open(type, url, true);
 
-    console.log(temperature);
-    if (FC) {
-        temp = (temperature * (9/5)) - 459.67;
-        hi = (high * (9/5)) - 459.67;
-        lo = (low * (9/5)) - 459.67;
-        console.log(temp);
-        document.getElementById('temp_display').innerHTML = "<p>" + temp.toPrecision(4) + "<p>";
-        document.getElementById('temphi_display').innerHTML= "<p>" + hi.toPrecision(4) + "<p>";
-        document.getElementById('templo_display').innerHTML= "<p>" + lo.toPrecision(4) + "<p>";
-        FC = false; 
-        button.value = 'F';
+            request.onload = function() {
+                if(request.status >= 200 && request.status <400) {
+                    //Success
+                     var data = JSON.parse(request.responseText);
+                     api.displayData(data);
+                } else {
+                    console.log('There was an error when trying to request OpenWeather api data.')
+                }
+            };
+            request.onerror = function() {
+                console.log('There was a connection error.')
+            }
+
+            request.send();        
+    },
+    Data: {},
+    setData: function(data) {
+        this.Data = Object.assign({}, data);
+    },
+    getData: function() {
+        return this.Data;
+    },
+    convertUnit: function () {
+            var new_temp;
+            if (FC) {
+                new_temp = (temperature * (9/5)) - 459.67;
+                api.setFC = false;
+                api.setButton('F');
+            }
+            else {
+                new_temp = temperature - 273.15;
+                api.setFC = true;
+                api.setButton('C');
+            } 
+            return new_temp;
+    },
+    displayData: function(data){
+        var weatherCity = data.name;
+        var weatherId = data.weather[0].id;
+        var weatherDesc = data.weather[0].description;
+        var weatherParams = data.weather[0].main;
+        var weatherTemp = data.main.temp;
+        var weatherHumid = data.main.humidity;
+        var tempHigh = data.main.temp_max;
+        var tempLow = data.main.temp_min;
+        var wndSpd = data.wind.speed;
+        var wndDeg = data.wind.deg;
+
+        var initialTemp = (weatherTemp * (9/5)) - 459.67;
+        var initialHigh = (tempHigh * (9/5)) - 459.67;
+        var initialLow = (tempLow * (9/5)) - 459.67;
+
+        document.getElementById('temp_display').innerHTML = "<p>" + initialTemp.toPrecision(4) + "<p>";
+        document.getElementById('humidity_display').innerHTML= "<p>" + weatherHumid + "<p>";
+        document.getElementById('temphi_display').innerHTML= "<p>" + initialHigh.toPrecision(4) + "<p>";
+        document.getElementById('templo_display').innerHTML= "<p>" + initialLow.toPrecision(4) + "<p>";
+        document.getElementById('city_display').innerHTML= "<p>" + weatherCity + "<p>";
+        this.weatherCodes(weatherId);
+    },
+    weatherCodes: function(code){
+        var display = document.getElementById('weather_display');
+        if (code >= 200 && code <= 232 ) {
+            display.innerHTML = "<p>This is a thunderstorm</p>";
+        }
+        else if (code >= 300 && code <= 321) {
+            display.innerHTML = "<p>This is drizzle</p>";
+        }
+        else if (code >= 500 && code <= 531) {
+            display.innerHTML = "<p>This is rain</p>";
+        }
+        else if (code >= 600 && code <= 622) {
+            display.innerHTML = "<p>This is Snow</p>";
+        }
+        else if (code == 800) {
+            display.innerHTML = "<p>This is clear sky</p>"; 
+        }
+        else if (code >= 801 && code <= 804) {
+            display.innerHTML = "<p>It is cloudy</p>"; 
+        }
+        else if (code >= 900 && code <= 906) {
+            display.innerHTML = "<p>Omg! Weather!</p>"; 
+        }
     }
-    else {
-        temp = temperature - 273.15;
-        hi = high - 273.15;
-        lo = low - 273.15;
-        console.log(temp);
-        document.getElementById('temp_display').innerHTML = "<p>" + temp.toPrecision(4) + "<p>";
-        document.getElementById('temphi_display').innerHTML= "<p>" + hi.toPrecision(4) + "<p>";
-        document.getElementById('templo_display').innerHTML= "<p>" + lo.toPrecision(4) + "<p>";
-        FC = true;
-        button.value = 'C';
-    }
-}
+};

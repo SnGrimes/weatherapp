b8ce4f2a865a52b6c65d80f960c3a0e01094e0ed
Shantia <shantiagrimes@gmail.com>
2017-06-08 17:40:01 -0500

    function edits

--- JS/script.js
+++ JS/script.js
@@ -59,7 +59,7 @@ var api = {
     setForecast: function(data) {
         this.Forecast = Object.assign({}, data);
     },
-    getForecast: function(data) {
+    getForecast: function() {
         return this.Forecast;
     },
     convertUnit: function (value) {
@@ -82,7 +82,6 @@ var api = {
         var low = parseInt(this.Data.main.temp_min);
         var button = document.getElementById('unit_switch');
 
-        console.log(temperature);
         if (this.FC) {
             //temp = (temperature * (9/5)) - 459.67;
             //hi = (high * (9/5)) - 459.67;
@@ -90,7 +89,7 @@ var api = {
             temp = this.convertUnit(temperature);
             hi = this.convertUnit(high);
             lo = this.convertUnit(low);
-            console.log(temp);
+
             document.getElementById('temp_display').innerHTML = "<p>" + temp.toPrecision(4) + "<p>";
             document.getElementById('temphi_display').innerHTML= "<p>" + hi.toPrecision(4) + "<p>";
             document.getElementById('templo_display').innerHTML= "<p>" + lo.toPrecision(4) + "<p>";
@@ -105,7 +104,7 @@ var api = {
             }
             var dataDate = 1;
             for (var i = 0; i < fiveDate.length; i++) {
-                fiveDate[i].textContent = this.Forecast.list[dataDate].dt_txt;
+                fiveDate[i].textContent = this.dateFormat(this.Forecast.list[dataDate].dt_txt);
                 dataDate += 8;
             }
             this.FC = false; 
@@ -133,7 +132,7 @@ var api = {
             }
             var dataDate = 1;
             for (var i = 0; i < fiveDate.length; i++) {
-                fiveDate[i].textContent = this.Forecast.list[dataDate].dt_txt;
+                fiveDate[i].textContent = this.dateFormat(this.Forecast.list[dataDate].dt_txt);
                 dataDate += 8;
             }
 
@@ -153,18 +152,21 @@ var api = {
         var wndSpd = data.wind.speed;
         var wndDeg = data.wind.deg;
 
-        document.getElementById('temp_display').innerHTML = "<p>" + this.convertUnit(weatherTemp).toPrecision(4) + "<p>";
-        document.getElementById('humidity_display').innerHTML= "<p>" + weatherHumid + "<p>";
-        document.getElementById('temphi_display').innerHTML= "<p>" + this.convertUnit(tempHigh).toPrecision(4) + "<p>";
-        document.getElementById('templo_display').innerHTML= "<p>" + this.convertUnit(tempLow).toPrecision(4) + "<p>";
-        document.getElementById('city_display').innerHTML= "<p>" + weatherCity + "<p>";
-        document.getElementById('weather_display').innerHTML = this.weatherCodes(weatherId);
+        document.getElementById('temp_display').innerHTML = "<p>" + this.convertUnit(weatherTemp).toPrecision(4) + "</p>";
+        document.getElementById('humidity_display').innerHTML= "<p>" + weatherHumid + "</p>";
+        document.getElementById('temphi_display').innerHTML= "<p>" + this.convertUnit(tempHigh).toPrecision(4) + "</p>";
+        document.getElementById('templo_display').innerHTML= "<p>" + this.convertUnit(tempLow).toPrecision(4) + "</p>";
+        document.getElementById('city_display').innerHTML= "<p>" + weatherCity + "</p>";
+        document.getElementById('weather_display').textContent = this.weatherCodes(weatherId);
+        document.getElementById('wind_spd').textContent = wndSpd;
+        document.getElementById('wind_dir').textContent = wndDeg;
         this.setData(data);
     },
     display5Day: function (data) {
         //console.log(data);
         var fiveDay = document.getElementsByClassName('day_fill');
         var fiveDate = document.getElementsByClassName('date_fill');
+        var list = [];
         
         var dataDay = 1;
         for (var j = 0; j < fiveDay.length; j++) {
@@ -173,33 +175,118 @@ var api = {
         }
         var dataDate = 1;
         for (var i = 0; i < fiveDate.length; i++) {
-            fiveDate[i].textContent = data.list[dataDate].dt;
+            fiveDate[i].textContent = this.dateFormat(data.list[dataDate].dt_txt);
             dataDate += 8;
         }
         this.setForecast(data);
+        this.drawGraph(list);
+    },
+    drawGraph: function () {
+            var canvas = document.getElementById('canvas');
+            var ctx = canvas.getContext('2d');
+
+            var temps = []; 
+            var count;
+            var gridCol = 0;
+            var HEIGHT = 105; 
+            var WIDTH = 100;
+            var dataDay = 1;
+            
+
+            for (var i = 0; i < 5; i++) {
+                temps[i] = this.convertUnit(this.Forecast.list[dataDay].main.temp).toPrecision(4);
+                dataDay +=8;
+            }
+            console.log(temps);
+            
+            
+            ctx.moveTo(0, HEIGHT - temps[0]);
+            ctx.beginPath();
+            for (count = 0; count < temps.length; count++) {
+                ctx.lineTo(gridCol += WIDTH, HEIGHT - temps[count]);
+                ctx.arc(gridCol, HEIGHT - temps[count],3, 0, Math.PI*2, true);
+            }
+            ctx.lineWidth = 2;
+            ctx.strokeStyle = 'white';
+            ctx.lineCap = 'round';
+            ctx.lineJoin = 'round';
+            ctx.stroke();
+            ctx.fill(arc);
+            
+            function createPoint(ctx,x, y) {
+                ctx.arc(x,y,3,0, Math.PI*2,true);
+            }
+    },
+    dateFormat: function (info) {
+        var manip = info.toString();
+        var month;
+        var monthNum = manip.substring(5,7);
+
+        switch (monthNum) {
+            case '01': 
+                month = 'January';
+                break;
+            case '02':
+                month = 'February';
+                break;
+            case '03': 
+                month = 'March';
+                break;
+            case '04': 
+                month = 'April';
+                break;
+            case '05':
+                month = 'May';
+                break;
+            case '06':
+                month = 'June';
+                break;
+            case '07':
+                month = 'July';
+                break;
+            case '08':
+                month = 'August';
+                break;
+            case '09':
+                month = 'September';
+                break;
+            case '10':
+                month = 'October';
+                break;
+            case '11':
+                month = 'November';
+                break;
+            case '12':
+                month = 'December';
+                break;
+        }
+        var day = manip.substring(8,10);
+        return month + " " + day;
     },
     weatherCodes: function(code){
         var display;
+        var weatherBckgrd = document.getElementsByClassName('weather_temp');
         if (code >= 200 && code <= 232 ) {
-            display = "<p>This is a thunderstorm</p>";
+            display = "This is a thunderstorm";
         }
         else if (code >= 300 && code <= 321) {
-            display = "<p>This is drizzle</p>";
+            display = "This is drizzle";
         }
         else if (code >= 500 && code <= 531) {
-            display = "<p>This is rain</p>";
+            display = "This is rain";
         }
         else if (code >= 600 && code <= 622) {
-            display = "<p>This is Snow</p>";
+            display = "This is Snow";
         }
         else if (code == 800) {
-            display = "<p>This is a clear sky</p>"; 
+            display = "This is a clear sky"; 
+            weatherBckgrd.style.backgroundColor = 'blue';
         }
         else if (code >= 801 && code <= 804) {
-            display = "<p>It is cloudy</p>"; 
+            display = "It is cloudy"; 
         }
         else if (code >= 900 && code <= 906) { 
-            display = "<p>Omg! Weather!</p>"; 
+            display = "Omg! Weather!"; 
         }
         return display;
     }

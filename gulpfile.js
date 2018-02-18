14ccde1405d1b47c518434ef468e328bc2ae5b2e
Shantia <shantiagrimes@gmail.com>
2018-02-18 13:48:26 -0600

    branch to development

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+//Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        proxy: "localhost:80/weatherapp"
+    });
+    gulp.watch("*.html").on("change", reload);
+    gulp.watch("CSS/*.css").on("change", reload);
+    gulp.watch("JS/*.js").on("change", reload);
+
+});
+
+gulp.task('default', ['browser-sync']);
